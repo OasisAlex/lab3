0b93d003cb6f83f6d3ef7f769c73821a9dbe81f9
OasisAlex <114462126+OasisAlex@users.noreply.github.com>
2022-11-12 00:17:15 -0800

    filter

--- ListTests.java
+++ ListTests.java
@@ -5,15 +5,40 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.zip.CheckedInputStream;
 
+import javax.lang.model.util.ElementScanner14;
+
 class listfiltercheck implements StringChecker {
-    public boolean checkString(String s) {
-        
+    public boolean checkString(String s)
+    {
+        if (s == "g" || s == "s")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-    
+}
 
 public class ListTests{
+    @Test
+    public void filtercheck() { 
+        listfiltercheck sc = new listfiltercheck();
+        List<String> list1 = List.of("g","a","b","g");
+        List<String> result = List.of("g","g");
+        assertEquals(result, ListExamples.filter(list1,sc));
+    }
+
+    @Test
+    public void filtercheck1() { 
+        listfiltercheck sc = new listfiltercheck();
+        List<String> list1 = List.of("g","1","g","2", "s");
+        List<String> result = List.of("g","g", "s");
+        assertEquals(result, ListExamples.filter(list1,sc));
+    }
+
     @Test 
-    
 	public void testFilter1() {
         List<String> list1 = List.of("a", "b", "g");
         List<String> list2 = List.of("c", "f", "w");
@@ -30,4 +55,4 @@ public class ListTests{
 	}
 
     }
-}
+
